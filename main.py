da4ec4def48ddda76972cf5f6f6c35ae638af70c
Yusuke Katori <yusuke2930@gmail.com>
2021-04-18 10:02:13 +0900

    query parameter

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ class ModelName(str, Enum):
     resnet = 'resnet'
     lenet = 'lenet'
 
-
 app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
 
 @app.get("/users/me")
 async def read_user_me():
