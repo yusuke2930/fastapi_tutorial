59cf7726b60cf1f5c33871efc7bb180fc36462ea
Yusuke Katori <yusuke2930@gmail.com>
2021-04-18 10:24:51 +0900

    Type conversion of query parameters

--- main.py
+++ main.py
@@ -12,10 +12,15 @@ app = FastAPI()
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: str, q: Optional[str] = None):
+async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
     if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
+        item.update({"q": item_id})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
