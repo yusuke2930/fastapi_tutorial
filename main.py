cd65293cd3a03a4b1681222145535830cee0fc9e
Yusuke Katori <yusuke2930@gmail.com>
2021-04-25 17:29:11 +0900

    body fields

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from typing import List, Optional
 from enum import Enum
 from fastapi import FastAPI, Query, Path, Body
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Item(BaseModel):
     name: str
-    description: Optional[str] = None
-    price: float
+    description: Optional[str] = Field(
+        None,
+        title="The description of the item",
+        max_length=300
+    )
+    price: float = Field(..., gt=0, description="The price must be greater than zero")
     tax: Optional[float] = None
 
 class User(BaseModel):
