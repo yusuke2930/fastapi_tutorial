1d57799e2b4735bf426954c5da11f0451cb3b446
Yusuke Katori <yusuke2930@gmail.com>
2021-04-27 12:45:24 +0900

    extra data types

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime, time, timedelta
+from uuid import UUID
 from typing import List, Set, Optional
 from enum import Enum
 from fastapi import FastAPI, Query, Path, Body
@@ -78,6 +80,26 @@ async def read_items(
     if q:
         results.update({"q": q})
     return results
+
+@app.put("/items/{item_id}")
+async def read_items(
+    item_id: UUID,
+    start_datetime: Optional[datetime] = Body(None),
+    end_datetime: Optional[datetime] = Body(None),
+    repeat_at: Optional[time] = Body(None),
+    process_after: Optional[timedelta] = Body(None),
+):
+    start_process = start_datetime + process_after
+    duration = end_datetime - start_process
+    return {
+        "item_id": item_id,
+        "start_datetime": start_datetime,
+        "end_datetime": end_datetime,
+        "repeat_at": repeat_at,
+        "process_after": process_after,
+        "start_process": start_process,
+        "duration": duration,
+    }
 
 @app.post("/items/")
 async def create_item(item: Item):
