b4bb2072e3ff9e11cf6f2a715908741063033f95
Yusuke Katori <yusuke2930@gmail.com>
2021-05-01 13:20:35 +0900

    request files

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from datetime import datetime, time, timedelta
 from uuid import UUID
 from typing import List, Set, Optional
 from enum import Enum
-from fastapi import FastAPI, Query, Path, Body, Cookie, Header, Form
+from fastapi import FastAPI, Query, Path, Body, Cookie, Header, Form, File, UploadFile
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel, Field, HttpUrl, EmailStr
 
 class UserIn(BaseModel):
@@ -186,3 +187,28 @@ async def create_user(user: UserIn):
 @app.post("/login/")
 async def login(username: str = Form(...), password: str = Form(...)):
     return {"username": username}
+
+
+@app.post("/files/")
+async def create_files(files: List[bytes] = File(...)):
+    return {"file_sizes": [len(file) for file in files]}
+
+@app.post("/uploadfiles")
+async def create_upload_files(files: List[UploadFile] = File(...)):
+    return {"filenames": [file.filename for file in files]}
+
+@app.get("/")
+async def main():
+    content = """
+<body>
+<form action="/files/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
