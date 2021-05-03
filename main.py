3ccb4703d61e03d8b8e8fe190f9d40e7ec44fdbe
Yusuke Katori <yusuke2930@gmail.com>
2021-05-03 19:52:38 +0900

    put

--- main.py
+++ main.py
@@ -23,9 +23,11 @@ class Image(BaseModel):
     name: str
 
 class Item(BaseModel):
-    title: str
-    timestamp: datetime
+    name: Optional[str] = None
     description: Optional[str] = None
+    price: Optional[float] = None
+    tax: float = 10.5
+    tags: List[str] = []
 
 class Offer(BaseModel):
     name: str
@@ -53,172 +55,177 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 
 fake_db = {}
 
-items = {"foo": "The Foo Wrestlers"}
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
 
 
-@app.exception_handler(UnicornException)
-async def unicorn_exception_handler(request: Request, exc: UnicornException):
-    return JSONResponse(
-        status_code=418,
-        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
-    )
+# @app.exception_handler(UnicornException)
+# async def unicorn_exception_handler(request: Request, exc: UnicornException):
+#     return JSONResponse(
+#         status_code=418,
+#         content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
+#     )
+#
+# @app.get("/unicorns/{name}")
+# async def read_unicorn(name: str):
+#     if name == "yolo":
+#         raise UnicornException(name=name)
+#     return {"unicorn_name": name}
+#
+# @app.get(
+#     "/items/{item_id}/name",
+#     response_model=Item,
+#     response_model_include={"name", "description"},
+# )
+# async def read_item_name(item_id: str):
+#     return items[item_id]
+#
+# @app.get("/items/{item_id}/public", response_model=Item, response_model_exclude={"tax"})
+# async def read_item_public_data(item_id: str):
+#     return items[item_id]
+#
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str):
+#     if item_id not in items:
+#         raise HTTPException(status_code=404, detail="Item not found")
+#     return {"item": items[item_id]}
+#
+# @app.get("/items/")
+# async def read_items(ads_id: Optional[str] = Cookie(None), user_agent: Optional[str] = Header(None)):
+#     return {"ads_id": ads_id, "User-Agent": user_agent}
+#
+# @app.post(
+#     "/items/",
+#     response_model=Item,
+#     summary="Create an item",
+#     response_description="The created item",
+#     status_code=status.HTTP_201_CREATED,
+#     tags=["items"]
+# )
+# async def create_item(item: Item):
+#     """
+#         Create an item with all the information:
+#
+#         - **name**: each item must have a name
+#         - **description**: a long description
+#         - **price**: required
+#         - **tax**: if the item doesn't have tax, you can omit this
+#         - **tags**: a set of unique tag strings for this item
+#     """
+#     return item
+#
+# @app.get("/items/user/{item_id}")
+# async def read_user_item(item_id: str, needy: str):
+#     item = {"item_id": item_id, "needy": needy}
+#     return item
+#
+# @app.get("/items/{item_id}")
+# async def read_items(
+#     q: str, item_id: int = Path(..., title="The ID of the item to get", gt=0, le=1000)
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     return results
+#
+# @app.put("/items/{item_id}")
+# async def read_items(
+#     item_id: UUID,
+#     start_datetime: Optional[datetime] = Body(None),
+#     end_datetime: Optional[datetime] = Body(None),
+#     repeat_at: Optional[time] = Body(None),
+#     process_after: Optional[timedelta] = Body(None),
+# ):
+#     start_process = start_datetime + process_after
+#     duration = end_datetime - start_process
+#     return {
+#         "item_id": item_id,
+#         "start_datetime": start_datetime,
+#         "end_datetime": end_datetime,
+#         "repeat_at": repeat_at,
+#         "process_after": process_after,
+#         "start_process": start_process,
+#         "duration": duration,
+#     }
+#
+# @app.get("/users/me")
+# async def read_user_me():
+#     return {"user_id": "the current user"}
+#
+#
+# @app.get("/users/{user_id}")
+# async def read_user(user_id: str):
+#     return {"user_id": user_id}
+#
+# @app.get("/users/{user_id}/items/{item_id}")
+# async def read_user_item(user_id: int, item_id: str, q: Optional[str] = None, short: bool = False):
+#     item = {"item_id": item_id, "owner_id": user_id}
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update({"description": "This is an amazing item that has a long description"})
+#     return item
+#
+# @app.get("/models/{model_name}")
+# async def get_model(model_name: ModelName):
+#     if model_name == ModelName.alexnet:
+#         return {"model_name": model_name, "message": "Deep Learning FTW!"}
+#     if model_name.value == 'lenet':
+#         return {"model_name": model_name, "message": "LeCNN all the images"}
+#
+#     return {"model_name": model_name, "message": "Have some residuals"}
+#
+# @app.get("/files/{file_path:path}")
+# async def read_file(file_path: str):
+#     return {"file_path": file_path}
+#
+# @app.post("/images/multiple/")
+# async def create_multiple_images(images: List[Image]):
+#     return images
+#
+# # Don't do this in production!
+# @app.post("/user/", response_model=UserOut)
+# async def create_user(user: UserIn):
+#     return user
+#
+# @app.post("/login/")
+# async def login(username: str = Form(...), password: str = Form(...)):
+#     return {"username": username}
+#
+#
+# @app.post("/files/")
+# async def create_files(file: bytes = File(...), fileb: UploadFile = File(...), token: str = Form(...)):
+#     return {
+#         "file_size": len(file),
+#         "token": token,
+#         "fileb_content_type": fileb.content_type,
+#     }
+#
+# @app.post("/uploadfiles")
+# async def create_upload_files(files: List[UploadFile] = File(...)):
+#     return {"filenames": [file.filename for file in files]}
+#
+# @app.get("/")
+# async def main():
+#     content = """
+# <body>
+# <form action="/files/" enctype="multipart/form-data" method="post">
+# <input name="files" type="file" multiple>
+# <input type="submit">
+# </form>
+# <form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+# <input name="files" type="file" multiple>
+# <input type="submit">
+# </form>
+# </body>
+#     """
+#     return HTMLResponse(content=content)
 
-@app.get("/unicorns/{name}")
-async def read_unicorn(name: str):
-    if name == "yolo":
-        raise UnicornException(name=name)
-    return {"unicorn_name": name}
-
-@app.get(
-    "/items/{item_id}/name",
-    response_model=Item,
-    response_model_include={"name", "description"},
-)
-async def read_item_name(item_id: str):
-    return items[item_id]
-
-@app.get("/items/{item_id}/public", response_model=Item, response_model_exclude={"tax"})
-async def read_item_public_data(item_id: str):
-    return items[item_id]
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: str):
-    if item_id not in items:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return {"item": items[item_id]}
-
-@app.get("/items/")
-async def read_items(ads_id: Optional[str] = Cookie(None), user_agent: Optional[str] = Header(None)):
-    return {"ads_id": ads_id, "User-Agent": user_agent}
-
-@app.post(
-    "/items/",
-    response_model=Item,
-    summary="Create an item",
-    response_description="The created item",
-    status_code=status.HTTP_201_CREATED,
-    tags=["items"]
-)
-async def create_item(item: Item):
-    """
-        Create an item with all the information:
-
-        - **name**: each item must have a name
-        - **description**: a long description
-        - **price**: required
-        - **tax**: if the item doesn't have tax, you can omit this
-        - **tags**: a set of unique tag strings for this item
-    """
-    return item
-
-@app.put("/items/{id}")
-async def update_item(id: str, item: Item):
-    json_compatible_item_data = jsonable_encoder(item)
-    fake_db[id] = json_compatible_item_data
-
-@app.get("/items/user/{item_id}")
-async def read_user_item(item_id: str, needy: str):
-    item = {"item_id": item_id, "needy": needy}
-    return item
-
-@app.get("/items/{item_id}")
-async def read_items(
-    q: str, item_id: int = Path(..., title="The ID of the item to get", gt=0, le=1000)
-):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    return results
-
-@app.put("/items/{item_id}")
-async def read_items(
-    item_id: UUID,
-    start_datetime: Optional[datetime] = Body(None),
-    end_datetime: Optional[datetime] = Body(None),
-    repeat_at: Optional[time] = Body(None),
-    process_after: Optional[timedelta] = Body(None),
-):
-    start_process = start_datetime + process_after
-    duration = end_datetime - start_process
-    return {
-        "item_id": item_id,
-        "start_datetime": start_datetime,
-        "end_datetime": end_datetime,
-        "repeat_at": repeat_at,
-        "process_after": process_after,
-        "start_process": start_process,
-        "duration": duration,
-    }
-
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
-
-
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
-
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(user_id: int, item_id: str, q: Optional[str] = None, short: bool = False):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update({"description": "This is an amazing item that has a long description"})
-    return item
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-    if model_name.value == 'lenet':
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
-
-@app.post("/images/multiple/")
-async def create_multiple_images(images: List[Image]):
-    return images
-
-# Don't do this in production!
-@app.post("/user/", response_model=UserOut)
-async def create_user(user: UserIn):
-    return user
-
-@app.post("/login/")
-async def login(username: str = Form(...), password: str = Form(...)):
-    return {"username": username}
-
-
-@app.post("/files/")
-async def create_files(file: bytes = File(...), fileb: UploadFile = File(...), token: str = Form(...)):
-    return {
-        "file_size": len(file),
-        "token": token,
-        "fileb_content_type": fileb.content_type,
-    }
-
-@app.post("/uploadfiles")
-async def create_upload_files(files: List[UploadFile] = File(...)):
-    return {"filenames": [file.filename for file in files]}
-
-@app.get("/")
-async def main():
-    content = """
-<body>
-<form action="/files/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
-<input type="submit">
-</form>
-<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
-<input type="submit">
-</form>
-</body>
-    """
-    return HTMLResponse(content=content)
+@app.put("/items/{item_id}", response_model=Item)
+async def update_item(item_id: str, item: Item):
+    update_item_encoded = jsonable_encoder(item)
+    items[item_id] = update_item_encoded
+    return update_item_encoded
