04537b65acad2538e977ad24679c17e92e5327ab
Yusuke Katori <yusuke2930@gmail.com>
2021-05-05 03:23:00 +0900

    dependencies in path operation decorators

--- main.py
+++ main.py
@@ -69,6 +69,17 @@ def query_or_cookie_extractor(
     return q
 
 
+async def verify_token(x_token: str = Header(...)):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+
+
+async def verify_key(x_key: str = Header(...)):
+    if x_key != "fake-super-secret-key":
+        raise HTTPException(status_code=400, detail="X-Key header invalid")
+    return x_key
+
+
 app = FastAPI()
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
@@ -259,16 +270,21 @@ async def update_item(item_id: str, item: Item):
 
 
 
-@app.get("/items/")
-async def read_items(commons: CommonQueryParams = Depends()):
-    response = {}
-    if commons.q:
-        response.update({"q": commons.q})
-    items = fake_items_db[commons.skip : commons.skip + commons.limit]
-    response.update({"items": items})
-    return response
+# @app.get("/items/")
+# async def read_items(commons: CommonQueryParams = Depends()):
+#     response = {}
+#     if commons.q:
+#         response.update({"q": commons.q})
+#     items = fake_items_db[commons.skip : commons.skip + commons.limit]
+#     response.update({"items": items})
+#     return response
 
 
 @app.get("/items/")
 async def read_query(query_or_default: str = Depends(query_or_cookie_extractor)):
     return {"q_or_cookie": query_or_default}
+
+
+@app.get("/items/", dependencies=[Depends(verify_token), Depends(verify_key)])
+async def read_items():
+    return [{"item": "Foo"}, {"item": "Bar"}]
