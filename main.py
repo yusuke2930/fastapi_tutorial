32a85a14dc66fa53f06daedb1adb796b745f8e99
Yusuke Katori <yusuke2930@gmail.com>
2021-05-04 11:09:17 +0900

    patch

--- main.py
+++ main.py
@@ -224,8 +224,15 @@ items = {
 #     """
 #     return HTMLResponse(content=content)
 
-@app.put("/items/{item_id}", response_model=Item)
+@app.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: str):
+    return items[item_id]
+
+@app.patch("/items/{item_id}", response_model=Item)
 async def update_item(item_id: str, item: Item):
-    update_item_encoded = jsonable_encoder(item)
-    items[item_id] = update_item_encoded
-    return update_item_encoded
+    stored_item_data = items[item_id]
+    stored_item_model = Item(**stored_item_data)
+    update_data = item.dict(exclude_unset=True)
+    updated_item = stored_item_model.copy(update=update_data)
+    items[item_id] = jsonable_encoder(updated_item)
+    return updated_item
